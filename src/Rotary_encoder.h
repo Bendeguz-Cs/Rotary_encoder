fbd11b6798927838ccf19552431b0b439aa9b4c8
Bendeguz <csermendy.bendeguz@icloud.com>
2026-05-26 22:30:40 +0200

    Add LED ring support to Rotary_encoder.h

--- src/Rotary_encoder.h
+++ src/Rotary_encoder.h
@@ -8,6 +8,15 @@
 
 #include "Arduino.h"
 
+#define USE_LED_RING  // Comment this out to disable LED ring support
+
+#ifdef USE_LED_RING
+  #include <Adafruit_NeoPixel.h>
+  #warning "LED Ring enabled!"
+#else
+  #warning "LED Ring disabled! To enable, define USE_LED_RING before including Rotary_encoder.h"
+#endif
+
 #if defined(ARDUINO_ARCH_ESP32)
   #define ENCODER_ISR_ATTR IRAM_ATTR
 #elif defined(ARDUINO_ARCH_ESP8266)
@@ -16,13 +25,27 @@
   #define ENCODER_ISR_ATTR
 #endif
 
+#ifdef USE_LED_RING
+  enum PatternMode {
+    NO_PATTERN,
+    POSITION_DOT_PATTERN,
+    FILL_BAR_PATTERN
+  };
+#endif
+
 class Encoder {
   public:
     Encoder(int CLK_PIN, int DT_PIN);
     void begin();
+    void LEDRing(int ledPin, uint16_t ledCount);
+    #ifdef USE_LED_RING
+      void positionDotPattern(byte red, byte green, byte blue);
+      void fillBarPattern(byte red, byte green, byte blue);
+      void noPattern();
+    #endif
     void setDebounceTime(int debounce_time);
     bool motion();
-    bool lastMotionSince(int noMotionTime);
+    bool lastMotionSince(int noMotionTime); //returns if there hasn't been any motion for at least the amount of ms entered as the parameter
     long read();
     void limits();
     void noLimits();
@@ -42,6 +65,12 @@ class Encoder {
   private:
     int _CLK_PIN;
     int _DT_PIN;
+    int _LED_PIN;
+    int _LED_COUNT;
+    bool useLEDRing = false;
+    #ifdef USE_LED_RING
+      Adafruit_NeoPixel* ring = nullptr; // per-instance NeoPixel
+    #endif
     int _scale;
     bool _motion_state = false;
     volatile bool feedbackMotion = false;
@@ -57,6 +86,12 @@ class Encoder {
     int8_t _quadState = 0;
     int _debounce_time = 5; // Default debounce time in milliseconds
     volatile unsigned long lastDebounceTime = 0;
+
+    #ifdef USE_LED_RING
+      PatternMode _activePattern = NO_PATTERN;
+      byte _patternRed = 255, _patternGreen = 255, _patternBlue = 255;
+      void _updatePattern();
+    #endif
 
     void ENCODER_ISR_ATTR updateState();  // Private function to update the state
 };
