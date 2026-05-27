09f4494c68b3db99315d1fce1f7930ef82de5305
Bendeguz <csermendy.bendeguz@icloud.com>
2026-05-27 19:46:41 +0200

    Add NeoPixel fixed gradient bar pattern example

---
+++ NeoPixel_fixedGradientBarPattern/NeoPixel_fixedGradientBarPattern.ino
@@ -0,0 +1,16 @@
+#include <Rotary_encoder.h>
+
+Encoder encoder(4, 5);
+
+void setup() {
+  encoder.LEDRing(6, 20);
+  encoder.setLimits(0, 40);   // set the range
+  encoder.limits();   // enable limit enforcement
+  encoder.fixedGradientBarPattern(0, 25, 0, 25, 0, 0);
+  encoder.begin();
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.println(encoder.read());
+}
